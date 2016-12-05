27ab1691c522e613423123369cff5d3fa48addbd
josephcode773 <josephcode773@gmail.com>
2016-12-05 01:36:01 -0600

    third commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var twitchStreamers = ["OgamingSC2", "freecodecamp", "comster404"];
-var streamName = twitchStreamers[2];
+var streamName = twitchStreamers[0];
 var ericHolder = "";
 var streamsHtmlHead = "https://crossorigin.me/https://wind-bow.hyperdev.space/twitch-api/streams/";
 var usersHtmlHead = "https://crossorigin.me/https://wind-bow.hyperdev.space/twitch-api/users/";
@@ -12,10 +12,19 @@ var streamStatus = function() {
         if (ericHolder === null) {
             console.log("Offline")
         } else {
-            console.log(ericHolder.channel.status)
+            // console.log(ericHolder.channel.status)
+            //return ericHolder.channel.status;
+            debugger;
+            $("#sayWord").html(JSON.stringify(ericHolder.channel.status));
         }
     });
 };
 // var status
 // var logo
 /* streamer: OgamingSC2 -- status/currently_playing: json.stream.channel.status -- weblink: */
+// $("#newQuote").click(function () {
+//     $("#sayWord").text(streamStatus());
+// });
+
+$("#sayWord").html(JSON.stringify());
+//$("#quoteText").html(JSON.stringify(json.quoteText));
