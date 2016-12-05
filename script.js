7e279a8ac0a4051f982400c2d76e7ac90b38b171
josephcode773 <josephcode773@gmail.com>
2016-12-05 03:22:46 -0600

    something is broken...

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-var twitchStreamers = ["OgamingSC2", "freecodecamp", "comster404"];
-var streamName = twitchStreamers[0];
-var ericHolder = "";
 var streamsHtmlHead = "https://crossorigin.me/https://wind-bow.hyperdev.space/twitch-api/streams/";
-var usersHtmlHead = "https://crossorigin.me/https://wind-bow.hyperdev.space/twitch-api/users/";
-var streamStatus = function() {
-    $.getJSON(streamsHtmlHead+streamName, function(json) {
-        ericHolder = json.stream;
-        if (json.status === 404) {
-            console.log(json.message)
-        }
-        if (ericHolder === null) {
-            console.log("Offline")
-        } else {
-            // console.log(ericHolder.channel.status)
-            //return ericHolder.channel.status;
-            debugger;
-            $("#sayWord").html(JSON.stringify(ericHolder.channel.status));
-        }
-    });
-};
-// var status
-// var logo
-/* streamer: OgamingSC2 -- status/currently_playing: json.stream.channel.status -- weblink: */
-// $("#newQuote").click(function () {
-//     $("#sayWord").text(streamStatus());
-// });
 
-$("#sayWord").html(JSON.stringify());
-//$("#quoteText").html(JSON.stringify(json.quoteText));
+var streamerOne = {
+    name: "OgamingSC2",
+    currentState: function () {
+        $.getJSON(streamsHtmlHead + this.name, function (json) {
+            if (json.status === 404) {
+                console.log(json.message)
+            }
+            if (json.stream === null) {
+                console.log("Offline")
+            } else {
+                // console.log(ericHolder.channel.status)
+                // $("#sayWord").html(JSON.stringify(json.string.channel.status));
+                console.log(json.string.channel.status)
+            }
+        });
+    },
+    webLink: function () {
+        $.getJSON(streamsHtmlHead + this.name, function (webby) {
+            // return webby.string.channel.url
+            console.log(webby.string.channel.url)
+
+        })},
+    picture: function () {
+            $.getJSON(streamsHtmlHead + this.name, function (john) {
+                // return john.string.channel.logo
+                console.log(john.string.channel.logo)
+
+            })
+        }
+    };
