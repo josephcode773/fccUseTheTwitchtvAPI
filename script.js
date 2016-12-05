95078ea026e056489f35ea68b515df5a868c2cf1
josephcode773 <josephcode773@gmail.com>
2016-12-05 06:38:19 -0600

    changed header

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
-var streamsHtmlHead = "https://crossorigin.me/https://wind-bow.hyperdev.space/twitch-api/streams/";
-
-var streamerOne = {
-    name: "OgamingSC2",
-    currentState: function () {
-        $.getJSON(streamsHtmlHead + this.name, function (json) {
-            if (json.status === 404) {
-                console.log(json.message)
-            }
-            if (json.stream === null) {
-                console.log("Offline")
-            } else {
-                // console.log(ericHolder.channel.status)
-                // $("#sayWord").html(JSON.stringify(json.string.channel.status));
-                console.log(json.string.channel.status)
-            }
-        });
-    },
-    webLink: function () {
-        $.getJSON(streamsHtmlHead + this.name, function (webby) {
-            // return webby.string.channel.url
-            console.log(webby.string.channel.url)
-
-        })},
-    picture: function () {
-            $.getJSON(streamsHtmlHead + this.name, function (john) {
-                // return john.string.channel.logo
-                console.log(john.string.channel.logo)
-
-            })
+var twitchStreamers = ["OgamingSC2", "freecodecamp", "comster404"];
+var streamName = twitchStreamers[0];
+var ericHolder = "";
+var streamsHtmlHead = "https://cors-anywhere.herokuapp.com/https://wind-bow.hyperdev.space/twitch-api/streams/";
+var usersHtmlHead = "https://crossorigin.me/https://wind-bow.hyperdev.space/twitch-api/users/";
+var streamStatus = function () {
+    $.getJSON(streamsHtmlHead + streamName, function (json) {
+        ericHolder = json;
+        if (json.status === 404) {
+            console.log(json.message)
         }
-    };
+        if (json.stream === null) {
+            console.log("Offline")
+        } else {
+// console.log(ericHolder.channel.status)
+            $("#sayWord").html(JSON.stringify(json.stream.channel.status));
+        }
+    });
+};
